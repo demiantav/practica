ef52ca52a9eca1b1746e530950fe5b4aff8083ec
Demian Tavolaro <demiantavolaro6@gmail.com>
2022-07-07 20:17:54 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector("canvas");
+let pincel = canvas.getContext("2d");
+
+document.write(" que haces pelotudo");
+
+pincel.fillStyle= "lightgrey";
+pincel.fillRect(0,0,250,320);
+
+pincel.fillStyle= "green";
+pincel.fillRect(0,0,26,320);
